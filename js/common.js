1c8d2ceac4bd784aeef0463d026c60acfd9a1768
mobilecheng <mobilecheng@qq.com>
2020-02-14 23:15:36 +0700

    Refactor code

--- js/common.js
+++ js/common.js
@@ -30,14 +30,14 @@ function onClickSend(param) {
 
 // getAPIList
 function getAPIList() {
-    //
-    var api_id, type_id, api_name, description;
+    
+    var api_id, type_id, api_name, description, apiItem, p;
     let requestURL = 'json/api_list.json';
 
     // dynamic create api_list div.
     $.getJSON(requestURL, function (result) {
         // get [api_list] div
-        apiList = $("#api_list");
+        var apiList = $("#api_list");
 
         for (let index = 0; index < result.data.length; index++) {
             api_id   = result.data[index].id;
@@ -46,7 +46,7 @@ function getAPIList() {
             description = result.data[index].description;
 
             // create [a] element
-            var apiItem = document.createElement('a');
+            apiItem = document.createElement('a');
             apiItem.id = api_id;
             apiItem.href = '#';
             apiItem.setAttribute('type_id', type_id);
@@ -55,13 +55,13 @@ function getAPIList() {
             apiItem.innerText = api_name;
             apiList.append(apiItem);
     
-            var p = document.createElement('p');
+            p = document.createElement('p');
             apiList.append(p);
         }
     });
 }
 
-//
+// Invoke a api, show content.
 function callAPI(obj) {
     // 
     var api_name = obj.innerHTML;
@@ -80,6 +80,15 @@ function callAPI(obj) {
     document.getElementById("type_id").innerHTML = type_id;
 
     // First, delete children of input_para div
+    deleteInputParamDiv();
+
+    // dynamic create input parameters div area.
+    createInputParamDiv(js_func);
+}
+
+// delete children of input_para div
+function deleteInputParamDiv() {
+
     var parent = document.getElementById('input_para');
     var children_amount = parent.children.length;
     console.log('children = ' + children_amount);
@@ -90,12 +99,16 @@ function callAPI(obj) {
             parent.removeChild(parent.children[index]);
         }
     }
+}
 
-    // dynamic create input parameters div area.
+// dynamic create input parameters div area.
+function createInputParamDiv(js_func) {
+
     let requestURL = 'json/api_list.json';
+
     $.getJSON(requestURL, function (result) {
         // get [input_para] div
-        input_para = $("#input_para");
+        var input_para = $("#input_para");
 
         for (let index = 0; index < result.data.length; index++) {
             if (js_func == result.data[index].id) {
@@ -109,37 +122,50 @@ function callAPI(obj) {
 
                 // create [title] element
                 var top_title = document.createElement('p');
-                top_title.id = 'top_title';
                 top_title.innerText = 'Input Parameters:';
                 input_para.append(top_title);
     
                 // Parameters
-                for (let index = 0; index < arrParams.length; index++) {
-                    // create [param_title] element
-                    var param_title = document.createElement('b');
-                    param_title.innerText = arrParams[index].name + ' : ';
-                    input_para.append(param_title);
-    
-                    // create [input_box] element
-                    var input_box = document.createElement('input');
-                    input_box.id = arrParams[index].input;
-                    input_para.append(input_box);
-    
-                    // create buttons
-                    var arrButtons = arrParams[index].buttons;
-    
-                    for (let index = 0; index < arrButtons.length; index++) {
-                        innerText  = arrButtons[index].innerText;
-                        invokeFunc = arrButtons[index].onclick;
-
-                        // create [button] element
-                        var button_get_it = document.createElement('button');
-                        button_get_it.setAttribute('onclick', invokeFunc);
-                        button_get_it.innerText = innerText;
-                        input_para.append(button_get_it);
-                    }
-                }
+                createParamOfAPI(arrParams);
             }
         }
     });
+}
+
+// create parameter of each API.
+function createParamOfAPI(arrParams) {
+
+    var param_title, input_box;
+
+    for (let index = 0; index < arrParams.length; index++) {
+        // create [param_title] element
+        param_title = document.createElement('b');
+        param_title.innerText = arrParams[index].name + ' : ';
+        input_para.append(param_title);
+
+        // create [input box of param] element
+        input_box = document.createElement('input');
+        input_box.id = arrParams[index].input;
+        input_para.append(input_box);
+
+        createButtonOfParam(arrParams, index);
+    }
+}
+
+// create button of parameter
+function createButtonOfParam(arrParams, index) {
+
+    var innerText, invokeFunc;
+    var arrButtons = arrParams[index].buttons;
+
+    for (let index = 0; index < arrButtons.length; index++) {
+        innerText  = arrButtons[index].innerText;
+        invokeFunc = arrButtons[index].onclick;
+
+        // create [button] element
+        var button = document.createElement('button');
+        button.setAttribute('onclick', invokeFunc);
+        button.innerText = innerText;
+        input_para.append(button);
+    }
 }
