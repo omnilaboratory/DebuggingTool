e2e1a679ab62dcb874e93d4fb99eb63ba5c9eccf
254698748@qq.com <zhuer1982>
2020-02-14 15:28:19 +0800

    api js file

---
+++ js/api.js
@@ -0,0 +1,96 @@
+var wsAddress = "ws://127.0.0.1:60020/ws"
+var ws;
+var isConnectToOBD = false;
+var message = {
+    type: 0,
+    data: {}
+}
+var api = {};
+
+api.connectToOBD = function() {
+    console.info("connectToOBD", isConnectToOBD)
+    if (isConnectToOBD) {
+        return;
+    }
+    ws = new WebSocket(wsAddress);
+    ws.onopen = function() {
+        //当WebSocket创建成功时，触发onopen事件
+        ws.send("connect obd first"); //将消息发送到服务端
+        isConnectToOBD = true;
+    }
+
+    ws.onmessage = function(e) {　　 //当客户端收到服务端发来的消息时，触发onmessage事件，参数e.data包含server传递过来的数据
+        jsonData = JSON.parse(e.data)
+        console.info("get data from obd: ", jsonData);
+        if (jsonData.status == false) {
+            if (jsonData.type != 0) {
+                alert(jsonData.result);
+            }
+            return;
+        }
+        resultData = jsonData.result;
+        console.info("data:", resultData);
+        switch (jsonData.type) {
+            case 1:
+                api.OnLogin(resultData);
+                break;
+            case 1001:
+                api.OnGetNewAddress(resultData);
+                break;
+            case 1205:
+                api.OnListProperties(resultData);
+                break;
+            case -200:
+                api.OnSendNewAddressOnLogin(resultData);
+                break;
+            default:
+                break;
+        }
+    }
+
+    ws.sendData = function(msg) {
+        console.info("send msg: ", msg);
+        ws.send(JSON.stringify(msg));
+    }
+    ws.onerror = function(event) {
+        console.info(event.data)
+    };
+}
+
+/* type 1 login  */
+var isLogin = false;
+api.SendLogin = function(e) {
+    console.info(e)
+    if (isLogin == false) {
+        message.type = 1
+        data = {
+            "mnemonic": "unfold tortoise zoo hand sausage project boring corn test same elevator mansion bargain coffee brick tilt forum purpose hundred embody weapon ripple when narrow"
+        };
+        message.data = data;
+        ws.sendData(message)
+    }
+}
+api.OnLogin = function(jsonData) {
+    isLogin = true;
+}
+
+/* type 1205 */
+api.SendListProperties = function() {
+    message.type = 1205;
+    ws.sendData(message);
+}
+api.OnListProperties = function(jsonData) {}
+
+/* type 1001 get new address by omnicore */
+api.SendGetNewAddress = function() {
+    message.type = 1001;
+    ws.sendData(message);
+}
+api.OnGetNewAddress = function(jsonData) {}
+
+/* type -200 get new address by  mnemonic*/
+api.SendNewAddressOnLogin = function() {
+    message.type = -200;
+    ws.sendData(message);
+}
+api.OnSendNewAddressOnLogin = function(jsonData) {}
--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
-function login(param) {
+function onClickSend(param) {
     apiName = $("#api_name").html()
     console.info(apiName)
-    console.info($(param))
+    if (apiName == 'api_name') {
+        api.connectToOBD();
+        return;
+    }
+    if (isConnectToOBD == false) {
+        alert("not connetToOBD");
+        return;
+    }
+    let inputData = {};
+    switch (apiName) {
+        case 'LogIn':
+            /* TODO get the data from input data
+             */
+            api.SendLogin(inputData);
+            break;
+        case 'NewAddress':
+            api.SendGetNewAddress();
+            break;
+        case 'NewAddressWithMnemonic':
+            api.SendNewAddressOnLogin();
+            break;
+        default:
+            console.info(apiName, "not exsit");
+            break;
+    }
 }
