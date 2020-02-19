b0e20086c024a7c8058f8f3e21d47147c6560545
254698748@qq.com <zhuer1982>
2020-02-19 10:23:56 +0800

    MessageType

--- js/common.js
+++ js/common.js
@@ -1,40 +1,38 @@
 var obdApi = new ObdApi();
 
 function onClickSend(param) {
-
     //为了测试
     msgType = parseInt($('#msgType').val());
-    let enumMsgType = new MessageType();
     switch (msgType) {
-        case enumMsgType.MsgType_Error_0:
+        case MessageType.MsgType_Error_0:
             obdApi.connectToServer("1111", function(e) {
                 console.info(e);
             });
             break;
-        case enumMsgType.MsgType_UserLogin_1:
+        case MessageType.MsgType_UserLogin_1:
             obdApi.login("", function(e) {
                 console.info(e);
             });
             break;
-        case enumMsgType.MsgType_UserLogout_2:
+        case MessageType.MsgType_UserLogout_2:
             obdApi.logout();
             break;
-        case enumMsgType.MsgType_GetMnemonic_101:
+        case MessageType.MsgType_GetMnemonic_101:
             obdApi.getMnemonic(function(e) {
                 console.info(e);
             });
             break;
-        case enumMsgType.MsgType_Core_FundingBTC_1009:
+        case MessageType.MsgType_Core_FundingBTC_1009:
             let info = new BtcFundingInfo();
             obdApi.fundingBTC(info);
             break;
-        case enumMsgType.MsgType_Mnemonic_CreateAddress_N200:
+        case MessageType.MsgType_Mnemonic_CreateAddress_N200:
             obdApi.createAddressByMnemonic();
             break;
-        case enumMsgType.MsgType_Mnemonic_GetAddressByIndex_201:
+        case MessageType.MsgType_Mnemonic_GetAddressByIndex_201:
             obdApi.getAddressByIndexByMnemonic(1);
             break;
-        case enumMsgType.MsgType_Core_Omni_GetTransaction_1206:
+        case MessageType.MsgType_Core_Omni_GetTransaction_1206:
             txid = "c76710920860456dff2433197db79dd030f9b527e83a2e253f5bc6ab7d197e73";
             obdApi.getOmniTxByTxid(txid);
             break;
